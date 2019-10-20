2c6f3e052b3c8c769f6d9668b357f736c97a1eed
cunninghamr <ryanc1934@gmail.com>
2019-10-20 17:23:16 -0400

    [Task 2.5] Implemented Detection class. Completed app/detection.cpp so that unit tests pass and commented that file and include/detection.hpp

--- app/detection.cpp
+++ app/detection.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file detection.cpp
+ * @brief
+ * @author
+ * @copyright 2019
+ * Distributed under the BSD License (license terms found in LICENSE or at https://www.freebsd.org/copyright/freebsd-license.html)
+ */
+
 #include "detection.hpp"
 
 #include <opencv2/opencv.hpp>
@@ -5,12 +13,14 @@
 #include <vector>
 
 Detection::Detection() {
-
+  // set the default people detector SVM to the HOG detector
+  hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
 }
 
 std::vector<cv::Rect> Detection::detect(const cv::Mat& image) {
-  cv::Rect nothing;
-  std::vector<cv::Rect> empty = {nothing};
+  // detect humans and get the bounding boxes for each
+  std::vector<cv::Rect> detections;
+  hog.detectMultiScale(image, detections);
 
-  return empty;
+  return detections;
 }
--- include/detection.hpp
+++ include/detection.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file detection.hpp
+ * @brief
+ * @author
+ * @copyright 2019
+ * Distributed under the BSD License (license terms found in LICENSE or at https://www.freebsd.org/copyright/freebsd-license.html)
+ */
+
 #ifndef INCLUDE_DETECTION_HPP_
 #define INCLUDE_DETECTION_HPP_
 
@@ -5,12 +13,20 @@
 
 #include <vector>
 
-
+/**
+ * Performs Histogram of Gradients detection of humans in images using Support Vector Machines.
+ */
 class Detection {
  public:
-
+  /**
+   * Initializes the HOG descriptor.
+   */
   Detection();
-
+  /**
+   * Detects humans in images using HOG and the OpenCV SVM default people detector.
+   * @param image
+   * @return
+   */
   std::vector<cv::Rect> detect(const cv::Mat& image);
  private:
   cv::HOGDescriptor hog;
