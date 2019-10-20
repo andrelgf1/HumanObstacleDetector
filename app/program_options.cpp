1899880df3e8a5f1c5c668987e1b171d1a3f5a19
viki <psanhuez.s@gmail.com>
2019-10-20 14:28:50 -0400

    [Task 2.11] Implementation of Program Options class. Files editted are program_options.cpp and program_options.hpp. Implementation of class was done thanks to the Unit Tests. All tests pass.

--- app/program_options.cpp
+++ app/program_options.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file program_options.cpp
+ * @brief This class allows for command line arguments for file paths.
+ * @author Pablo Sanhueza
+ * @author Ryan Cunningham
+ * @author Andre Gomes
+ * @copyright 2019 Pablo Sanhueza, Ryan Cunningham, Andre Gomes
+ * Distributed under the BSD License (license terms found in LICENSE or at https://www.freebsd.org/copyright/freebsd-license.html)
+ */
+
 #include "program_options.hpp"
 
 #include <iostream>
@@ -5,18 +15,46 @@
 
 namespace po = boost::program_options;
 
-ProgramOptions::ProgramOptions() {
-
+ProgramOptions::ProgramOptions()
+    : description(po::options_description("Human Obstacle Detector")) {
+  /// add program options
+  description.add_options()("help,h", "Help Message")(
+      "positive-test-images-path,p", po::value<std::string>()->required(),
+      "Path to directory containing positive test images")(
+      "negative-test-images-path,n", po::value<std::string>()->required(),
+      "Path to directory containing negative test images");
 }
 
 void ProgramOptions::parse(int argc, char* argv[]) {
+  /// get and store the parsed options
+  po::store(po::command_line_parser(argc, argv).options(description).run(),
+            optionValues);
 
+  /// if no options provided, stop program and print options
+  if (optionValues.size() == 0) {
+    print();
+    throw std::invalid_argument("No options provided");
+  }
+
+  /// if help option provided, stop program and print options
+  if (optionValues.count("help")) {
+    print();
+    exit(0);
+  }
 }
 
 std::string ProgramOptions::getValue(const std::string& optionName) {
-  return "";
+  /// if the option is not provided, throw an exception and print the options
+  if (!optionValues.count(optionName)) {
+    print();
+    throw std::invalid_argument("Invalid option: " + optionName);
+  }
+
+  /// get the option value
+  return optionValues[optionName].as<std::string>();
 }
 
 void ProgramOptions::print() {
-
+  /// print to stdout
+  description.print(std::cout);
 }
--- include/program_options.hpp
+++ include/program_options.hpp
@@ -1,17 +1,47 @@
+/**
+ * Program Options
+ * @brief Class allows for command line arguments for setting path.
+ * @author Ryan Cunningham
+ * @author Pablo Sanhueza
+ * @author Andre Gomes
+ * @copyright 2019 Ryan Cunningham, Pablo Sanhueza, Andre Gomes
+ */
+
 #ifndef INCLUDE_PROGRAM_OPTIONS_HPP_
 #define INCLUDE_PROGRAM_OPTIONS_HPP_
 
 #include <boost/program_options.hpp>
 
+/**
+ * Defines available program options and retrieves option values.
+ */
 class ProgramOptions {
  public:
   ProgramOptions();
+
+  /**
+   * Parse command line arguments as options.
+   * @param argc the number of arguments
+   * @param argv the list of arguments
+   */
   void parse(int argc, char* argv[]);
+
+  /**
+   * Gets the value for an options
+   * @param optionName the name of the option
+   * @return the option value
+   */
   std::string getValue(const std::string& optionName);
+
  private:
   boost::program_options::options_description description;
   boost::program_options::variables_map optionValues;
+
+  /**
+   * Prints the options description to stdout.
+   */
   void print();
 };
 
 #endif /* INCLUDE_PROGRAM_OPTIONS_HPP_ */
+
