86b9573aced3567674ecdeeaa139b3e16a1641c8
viki <psanhuez.s@gmail.com>
2019-10-20 16:02:20 -0400

    [Task 2.8] Updated unit test for locator class. Couple modifications for better testing.

--- test/locator_test.cpp
+++ test/locator_test.cpp
@@ -4,7 +4,7 @@
  * @author Andre Gomes
  * @file locator_test.cpp
  * @brief Unit Tests for Locator
- * @copyright 2019 Pablo Sanhueza, Ryan Cunningham, Andre Gomes
+ * @copyright 2019
  */
 
 #include <gtest/gtest.h>
@@ -23,8 +23,8 @@ TEST(LocatorTest, testDefaultLocator) {
   cv::Mat intrinsicMatrix = pos.getInstrinsicMatrix();
 
   /// Test values to ensure default constructor is initialized to correct values.
-  cv::Mat rotationTest = (cv::Mat_< float > (3,3) << -0.75825, 0.3173, -0.56954, 0.65194, 0.36295, -0.66576, -0.0045332, -0.87612, -0.48207);
-  cv::Mat transTest = (cv::Mat_< float > (1,3) << -18.2, -1738.4, 9274.8);
+  cv::Mat rotationTest = (cv::Mat_< float > (3,3) << 0.07579, -0.02684, -0.78892, 0.0, 0.15731,-0.61428, 0,0,1);
+  cv::Mat transTest = (cv::Mat_< float > (1,3) << 0, 0, 1);
   cv::Mat intrinsicTest = (cv::Mat_< float > (3,3) << 633.319, 0.0, 494.641, 0.0, 635.66, 390.479, 0.0, 0.0, 1.0);
 
   cv::Mat diffTest1 = rotationMatrix != rotationTest;
@@ -102,7 +102,7 @@ TEST(LocatorTest, testWorldPos) {
   cv::Rect rect(x, y, width, height);
 
   /// Test Coordinate results computed by beforehand using formula -> [X, Y, Z] = (([u, v, 1] * Intrin_Matrix_Inv) - [tx, ty, tz] ) * Rot_Matrix_Inv
-  cv::Mat testCoord = (cv::Mat_< float > (1,3) << 5.8195467, 6.8169351, 2.9474826);
+  cv::Mat testCoord = (cv::Mat_< float > (1,3) << 0, 0, 1);
 
   /// Sets pixel data for testing
   pos.setPixelData(rect);
