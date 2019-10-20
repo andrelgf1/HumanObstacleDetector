4b0d2d907168fe60b337219ded6b5a5e55593c90
Andre Ferreira <andrelgferreira1@gmail.com>
2019-10-20 13:20:21 -0700

    Task[2.9] Implementation of loactor class , which was based on the locator unit tests. Files edited were the locator.cpp and locator.hpp

--- app/locator.cpp
+++ app/locator.cpp
@@ -1,3 +1,16 @@
+/** @author Pablo Sanhueza
+ *  @author Andre Gomes
+ *  @author Ryan Cunningham
+ *
+ *  @file locator.cpp
+ *
+ *  @brief End result of class is to return the position of the object desired. Class will receive dimensions and starting pixel
+ *  position of a bounding box, and from there determine the world coordinate position depending on the camera.
+ *  Here we assume the intrinsic and extrinsic parameters of the camera.
+ *
+ *  @copyright 2019 Pablo Sanhueza, Andre Gomes, Ryan Cunningham
+ */
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <string>
@@ -6,18 +19,27 @@
 
 
 Locator::Locator(){
-
+  /// rotation matrix is conformed of [r1 r2 translation]. r3 isn't necessary because it is a redundant cross product of the first 2 columns
+  _rotationM = (cv::Mat_< float > (3,3) << 0.07579, -0.02684, -0.78892, 0.0, 0.15731,-0.61428, 0,0,1);
+  _transVec = (cv::Mat_< float > (1,3) << 0, 0, 1);
+  _intrinsicM = (cv::Mat_< float > (3,3) << 633.319, 0.0, 494.641, 0.0, 635.66, 390.479, 0.0, 0.0, 1.0);
 }
 
 Locator::Locator(cv::Mat rotationM, cv::Mat translationVec, cv::Mat intrinsicM):
- _rotationM(rotationM),
-_transVec(translationVec),
-_intrinsicM(intrinsicM) {
+  _rotationM(rotationM),
+  _transVec(translationVec),
+  _intrinsicM(intrinsicM) {
 
+  /// Check size of matrix
+  checkMatrixSize(rotationM, cv::Size(3,3));
+  checkMatrixSize(translationVec, cv::Size(3,1));
+  checkMatrixSize(intrinsicM, cv::Size(3,3));
 }
 
 void Locator::checkMatrixSize(cv::Mat matrix, cv::Size size){
-
+  if ( matrix.size().width != size.width && matrix.size().height != size.height){
+    throw std::invalid_argument("Invalid Shape of Vector (Matrix shape (3x3) and Vector shape is (1x3)");
+  }
 }
 
 cv::Mat Locator::getRotationMatrix(){
@@ -32,11 +54,18 @@ cv::Mat Locator::getInstrinsicMatrix(){
 
 
 void Locator::pixelVector() {
+  /// Uses the public attribute of cv::Rec object to obtain width and height of bounding box.
+  int midX = pixelPoint.width / 2;
+  int midY = pixelPoint.height;
 
+  /// Gets pixel coordinate and converts to a 1x3 vector.
+  pixelCoord = (cv::Mat_<float>(1,3) << pixelPoint.x + midX,  pixelPoint.y + midY, 1);
 }
 
 void Locator::setPixelData(cv::Rect &pixelData) {
-
+  /// Sets pixelData to attribute pixelPoint
+  pixelPoint = pixelData;
+  pixelVector();
 }
 
 cv::Rect Locator::getPixelData() {
@@ -45,9 +74,17 @@ cv::Rect Locator::getPixelData() {
 
 
 void Locator::worldPos() {
+  cv::Mat temp;
+
+  /// Calculates real world coordinate with intrinsic and extrinsic camera parameters.
+ /// worldCoord = (((pixelCoord * _intrinsicM.inv()) - _transVec ) * _rotationM.inv()) / 1000;
+
+  cv::Mat homography = _intrinsicM*_rotationM;
+  worldCoord = pixelCoord * homography.inv();
 
 }
 
 void Locator::printPositions() {
-
+  /// Prints position of object detected (human in this case).
+  std::cout << "Position of Object in meters [x,y,z]: " << worldCoord << std::endl;
 }
